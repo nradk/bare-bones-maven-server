d510a77568189b16869c46961986a49d04334d92
Neeraj Adhikari <adh.neeraj@gmail.com>
2018-07-20 11:46:41 +0545

    Use HTTP basic authentication with http-auth
    Use http-auth to implement http basic authentication for the server.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
-const app = express();
+const auth = require('http-auth');
 
+const app = express();
+const basic = auth.basic({
+    realm: "Credentials required for access to MavenServer",
+    file: __dirname + "/.htpasswd"
+});
+
+app.use(auth.connect(basic));
 app.use(express.static('maven'));
 
 app.listen(3000, () => console.log("Maven server listening on port 3000!"));
-
